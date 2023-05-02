d1c89140fb968ab0c048d3da54d5bb9ec0f8da82
Alejandro Villarreal <m.ale.villarreal.0803@gmail.com>
2023-05-01 23:17:12 -0300

    modi en las rutas de productos ABM

--- controllers/product.controller.js
+++ controllers/product.controller.js
@@ -1,21 +1,26 @@
 const express = require('express');
 const { query, validationResult } = require('express-validator');
 
-const { obtenerProductos, obtenerProductoPorId, crearProducto, editarProducto, disableProducto, eliminarProdcucto } = require('../services/product.service');
+const { obtenerProductos, obtenerProductoPorId, crearProducto, editarProducto, disableProducto, eliminarProducto } = require('../services/product.service');
 
 
-const getProducts = async () => {
+const getProducts = async (req, res) => {
     try {
-
+        const resp = await obtenerProductos();
+        if (resp.length === 0) return res.status(404).json('No hay productos en la Base de Datos.');
+        res.status(200).json(resp);
     } catch (error) {
-
+        res.status(500).json(error.message);
     }
 };
-const getProductById = async () => {
+const getProductById = async (req, res) => {
     try {
-
+        const { id } = req.params;
+        const resp = await obtenerProductoPorId(id);
+        if (!resp) return res.status(404).json('Producto no encontrado.');
+        res.status(200).json(resp);
     } catch (error) {
-
+        res.status(500).json(error.message);
     }
 };
 const createProduct = async (req, res) => {
@@ -27,25 +32,32 @@ const createProduct = async (req, res) => {
         res.status(500).json(error.message);
     }
 };
-const editProduct = async () => {
+const editProduct = async (req, res) => {
     try {
-
+        const { id } = req.params;
+        const productData = req.body;
+        const resp = await editarProducto(id, productData);
+        if (!resp) return res.status(404).json('Producto no encontrado.');
+        res.status(200).json(resp);
     } catch (error) {
-
+        res.status(500).json(error.message);
     }
 };
-const disableProduct = async () => {
+const disableProduct = async (req, res) => {
     try {
-
+        res.status(200).json("NO implentado todavia");
     } catch (error) {
-
+        res.status(500).json(error.message);
     }
 };
-const deleteProduct = async () => {
+const deleteProduct = async (req, res) => {
     try {
-
+        const { id } = req.params;
+        const resp = await eliminarProducto(id);
+        if (!resp) return res.status(404).json('Producto no encontrado.');
+        res.status(200).json(resp);
     } catch (error) {
-
+        res.status(500).json(error.message);
     }
 };
 
--- routes/produc.routes.js
+++ routes/produc.routes.js
@@ -1,13 +1,13 @@
-const {Router}= require('express')
-const route= Router();
-const {getProducts,getProductById,createProduct,editProduct, disableProduct, deleteProduct}= require('../controllers/product.controller');
+const { Router } = require('express')
+const route = Router();
+const { getProducts, getProductById, createProduct, editProduct, disableProduct, deleteProduct } = require('../controllers/product.controller');
 
 
 route.get('/get-products', getProducts);
-route.get('/get-product-byId/:id', getProductById);
+route.get('/get-product-by-Id/:id', getProductById);
 route.post('/create-product', createProduct);
 route.patch('/edit-product/:id', editProduct);
 route.patch('/disable-product/:id', disableProduct);
 route.delete('/delete-product/:id', deleteProduct);
 
-module.exports= route;
+module.exports = route;
