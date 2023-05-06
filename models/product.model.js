2d228e79d86d28652d426f6b7007474ec6aaa08f
adrianffigueroa <adrianffigueroa@gmail.com>
2023-05-05 21:55:31 -0300

    agrego campo de codigo al esquema de productos

--- models/product.model.js
+++ models/product.model.js
@@ -1,20 +1,24 @@
-const { Schema, model } = require('mongoose');
+const { Schema, model } = require("mongoose");
 
 const producSchema = Schema({
-    destacado: { type: Boolean, default: false },
-    categoria: String,
-    subCategoria: String,
-    marca: String,
-    resumenDescripcion: String,
-    descripcion: String,
-    imagen: String,
-    color: String,
-    precio: Number,
-    cantidadPorTalle: {
-        xs: Number, m: Number, l: Number, xl: Number,
-        s: Number
-    },
-    clase: []
+  destacado: { type: Boolean, default: false },
+  codigo: String,
+  categoria: String,
+  subCategoria: String,
+  marca: String,
+  resumenDescripcion: String,
+  descripcion: String,
+  imagen: String,
+  color: String,
+  precio: Number,
+  cantidadPorTalle: {
+    xs: Number,
+    m: Number,
+    l: Number,
+    xl: Number,
+    s: Number,
+  },
+  clase: [],
 });
 
-module.exports = model('vestire-product', producSchema);
+module.exports = model("vestire-product", producSchema);
