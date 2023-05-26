8ddee6642d47028b3cd76561186a635442a071ac
adrianffigueroa <adrianffigueroa@gmail.com>
2023-05-26 11:28:07 -0300

    corrigiendo busqueda por email

--- services/user.service.js
+++ services/user.service.js
@@ -1,48 +1,48 @@
-const User = require('../models/user.model');
+const User = require("../models/user.model");
 
 const obtenerUsuarios = async () => {
-    return await User.find({});
+  return await User.find({});
 };
 
 const obtenerUsuarioPorId = async (id) => {
-    return await User.findById(id);
+  return await User.findById(id);
 };
 
 const obtenerUsuarioPorEmail = async (emailFind) => {
-    const email = emailFind;
-    return await User.findOne(email);
+  const email = emailFind;
+  return await User.findOne({ email });
 };
 
 const crearUsuario = async (user) => {
-    const newUser = new User(user)
-    return await newUser.save()
+  const newUser = new User(user);
+  return await newUser.save();
 };
 
 const editarUsuario = async (id, userData) => {
-    return await User.findByIdAndUpdate(id, userData);
+  return await User.findByIdAndUpdate(id, userData);
 };
 
 const eliminarUsuario = async (id) => {
-    return await User.findByIdAndDelete(id);
+  return await User.findByIdAndDelete(id);
 };
 
 const usuarioEsAdmin = async (id) => {
-    const user = await obtenerUsuarioPorId(id);
-    return user.admin;
+  const user = await obtenerUsuarioPorId(id);
+  return user.admin;
 };
 
 const Info = async (id) => {
-    const user = await obtenerUsuarioPorId(id);
-    return user;
+  const user = await obtenerUsuarioPorId(id);
+  return user;
 };
 
 module.exports = {
-    obtenerUsuarios,
-    obtenerUsuarioPorId,
-    obtenerUsuarioPorEmail,
-    crearUsuario,
-    editarUsuario,
-    eliminarUsuario,
-    usuarioEsAdmin,
-    Info
+  obtenerUsuarios,
+  obtenerUsuarioPorId,
+  obtenerUsuarioPorEmail,
+  crearUsuario,
+  editarUsuario,
+  eliminarUsuario,
+  usuarioEsAdmin,
+  Info,
 };
