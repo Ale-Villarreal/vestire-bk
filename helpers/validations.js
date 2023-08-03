46728206c447df2151fa4ff2973aa22129debb3b
adrianffigueroa <adrianffigueroa@gmail.com>
2023-08-03 13:53:37 -0300

    bloqueo de login usuario deshabilitado

--- helpers/validations.js
+++ helpers/validations.js
@@ -1,55 +1,59 @@
-const User = require('../models/user.model');
-const jwt = require('jsonwebtoken');
-const bcrypt = require("bcrypt");
+const User = require('../models/user.model')
+const jwt = require('jsonwebtoken')
+const bcrypt = require('bcrypt')
 
 const emailExists = async (email) => {
-  const searchEmail = await User.find({ email });
+  const searchEmail = await User.find({ email })
   if (searchEmail.length !== 0) {
-    throw new Error(`El email ${email}, ya se encuentra registrado.`);
+    throw new Error(`El email ${email}, ya se encuentra registrado.`)
   }
   return false
-};
+}
+
+const userDisable = async () => {
+  const userIsDisabled = await User.find({ disable })
+  if (userIsDisabled) {
+    throw new Error(
+      'El usuario se encuentra deshabilitado. Ponganse en contacto con el administrador.'
+    )
+  }
+}
 
 const generateToken = (data) => {
-
-  const token = jwt.sign(
-    { data },
-    process.env.TOKEN_SECRET,
-    { expiresIn: "1d" }
-  );
-  return token;
-
-};
+  const token = jwt.sign({ data }, process.env.TOKEN_SECRET, {
+    expiresIn: '1d',
+  })
+  return token
+}
 
 const getUserDataByToken = (authHeader) => {
   try {
     if (!authHeader) {
-      console.log("Debe de proporcionar un Token en la solicitud.");
+      console.log('Debe de proporcionar un Token en la solicitud.')
     }
-    const token = authHeader.split(" ")[1];
-    const { data } = jwt.verify(token, process.env.TOKEN_SECRET);
-    return data;
+    const token = authHeader.split(' ')[1]
+    const { data } = jwt.verify(token, process.env.TOKEN_SECRET)
+    return data
   } catch (error) {
     console.log(error)
   }
-
 }
 
 const generateUUID = () => {
-  var d = new Date().getTime();
+  var d = new Date().getTime()
   var uuid = 'xxxxxxxxxxxx4xxxyxxxxxxxxxxxxxxx'.replace(/[xy]/g, function (c) {
-    var r = (d + Math.random() * 16) % 16 | 0;
-    d = Math.floor(d / 16);
-    return (c == 'x' ? r : (r & 0x3 | 0x8)).toString(16);
-  });
-  return uuid;
-};
+    var r = (d + Math.random() * 16) % 16 | 0
+    d = Math.floor(d / 16)
+    return (c == 'x' ? r : (r & 0x3) | 0x8).toString(16)
+  })
+  return uuid
+}
 
 const generateHash = (text) => {
-  const saltRound = 10;
-  const salt = bcrypt.genSaltSync(saltRound);
-  const pswHash = bcrypt.hashSync(text, salt);
-  return pswHash;
+  const saltRound = 10
+  const salt = bcrypt.genSaltSync(saltRound)
+  const pswHash = bcrypt.hashSync(text, salt)
+  return pswHash
 }
 
 module.exports = {
@@ -57,8 +61,8 @@ module.exports = {
   generateToken,
   generateUUID,
   getUserDataByToken,
-  generateHash
+  generateHash,
+  userDisable,
 }
-
 
 // /(?=(.*[0-9]))(?=.*[\!@#$%^&*()\\[\]{}\-_+=|:;"'<>,./?])(?=.*[a-z])(?=(.*[A-Z]))(?=(.*)).{8,}/
--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,6 +1,6 @@
-const { Router } = require("express");
-const route = Router();
-const { body, param } = require("express-validator");
+const { Router } = require('express')
+const route = Router()
+const { body, param } = require('express-validator')
 const {
   getUsers,
   getUserById,
@@ -14,93 +14,88 @@ const {
   getUserByEmail,
   getUserByUserName,
   forgotPassword,
-  resetPassword
-} = require("../controllers/user.controller");
-const { emailExists } = require("../helpers/validations");
-const { authMiddleware } = require("../routes/validate-token");
+  resetPassword,
+} = require('../controllers/user.controller')
+const { emailExists, userDisable } = require('../helpers/validations')
+const { authMiddleware } = require('../routes/validate-token')
 const msgPassword =
-  "La contraseña de contener al menos: una letra, una letra mayúscula, un número, y minimo 5 caracteres.";
+  'La contraseña de contener al menos: una letra, una letra mayúscula, un número, y minimo 5 caracteres.'
 
-route.get("/get-users", getUsers);
+route.get('/get-users', getUsers)
 
 route.post(
-  "/login",
+  '/login',
   // Debe de ingresar el nombre de usuario
-  body("username")
+  body('username')
     .not()
     .isEmpty()
-    .withMessage("Debe de ingresar el nombre de usuario."),
+    .withMessage('Debe de ingresar el nombre de usuario.'),
   // debe ser un correo electrónico valido
-  body("email")
+  body('email')
     .isEmail()
-    .withMessage("Formato de correo electrónico no valido.")
+    .withMessage('Formato de correo electrónico no valido.')
     .not()
     .isEmpty()
-    .withMessage("El campo email esta vacio."),
+    .withMessage('El campo email esta vacio.'),
   // Requerimientos de conytraña
-  body("password")
+  body('password')
     .matches(
       /(?=(.*[0-9]))(?=.*[\!@#$%^&*()\\[\]{}\-_+=|:;"'<>,./?])(?=.*[a-z])(?=(.*[A-Z]))(?=(.*)).{5,}/
     )
     .withMessage(msgPassword),
+  custom(userDisable),
   loginUser
-);
+)
 
-route.get("/get-user-by-id/:id", getUserById);
+route.get('/get-user-by-id/:id', getUserById)
 
-route.get("/get-user-by-token", authMiddleware, getUserById);
+route.get('/get-user-by-token', authMiddleware, getUserById)
 
 route.post(
-  "/create-user",
+  '/create-user',
   // Debe de ingresar el nombre de usuario
-  body("username")
+  body('username')
     .not()
     .isEmpty()
-    .withMessage("Debe de ingresar el nombre de usuario."),
+    .withMessage('Debe de ingresar el nombre de usuario.'),
   // Debe de ingresar su Nombre
-  body("firstname")
-    .not()
-    .isEmpty()
-    .withMessage("Debe de ingresar su Nombre."),
+  body('firstname').not().isEmpty().withMessage('Debe de ingresar su Nombre.'),
   // Debe de ingresar su Nombre
-  body("lastname")
-    .not()
-    .isEmpty()
-    .withMessage("Debe de ingresar su Apellido."),
+  body('lastname').not().isEmpty().withMessage('Debe de ingresar su Apellido.'),
   // debe ser un correo electrónico valido
-  body("email")
+  body('email')
     .isEmail()
-    .withMessage("Formato de correo electrónico no valido.")
+    .withMessage('Formato de correo electrónico no valido.')
     .not()
     .isEmpty()
-    .withMessage("El campo email esta vacio.")
+    .withMessage('El campo email esta vacio.')
     .custom(emailExists),
   // Requerimientos de conytraña
-  body("password")
+  body('password')
     .matches(
       /(?=(.*[0-9]))(?=.*[\!@#$%^&*()\\[\]{}\-_+=|:;"'<>,./?])(?=.*[a-z])(?=(.*[A-Z]))(?=(.*)).{5,}/
     )
     .withMessage(msgPassword),
   createUser
-);
+)
 
-route.patch("/edit-user/:id", editUser);
+route.patch('/edit-user/:id', editUser)
 
-route.patch("/disable-user/:id", disableUser);
+route.patch('/disable-user/:id', disableUser)
 
-route.delete("/delete-user/:id", deleteUser);
+route.delete('/delete-user/:id', deleteUser)
 
-route.get("/check-user-admin", isAdmin);
+route.get('/check-user-admin', isAdmin)
 
-route.get("/info", authMiddleware, info);
+route.get('/info', authMiddleware, info)
 
-route.get("/get-user-by-email/:email", getUserByEmail);
+route.get('/get-user-by-email/:email', getUserByEmail)
 
-route.get("/get-user-by-username/:username", getUserByUserName);
+route.get('/get-user-by-username/:username', getUserByUserName)
 
 // Ruta para manejar la solicitud de recuperación de contraseña
-route.post('/forgot-password', forgotPassword);
+route.post('/forgot-password', forgotPassword)
 // Ruta para manejar el restablecimiento de la contraseña
-route.post('/reset-password', authMiddleware, resetPassword);
+route.post('/reset-password', authMiddleware, resetPassword)
 
-module.exports = route;
+module.exports = route
